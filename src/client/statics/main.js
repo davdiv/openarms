9563a87ab2e4abb11b377e0b1c6b30ab98e311d6
DivDE <divde@free.fr>
2014-04-13 23:18:58 +0200

    Adding a confirmation when closing.

--- src/client/statics/main.js
+++ src/client/statics/main.js
@@ -1,3 +1,8 @@
 var tabMgr = require("./tabMgr");
 var main = require("./main.hsp.js");
 main(tabMgr).render(document.body);
+
+window.onbeforeunload = function (event) {
+    var res = event.returnValue = "Cette opération va fermer Openarms et les éventuelles données non enregistrées seront perdues.";
+    return res;
+};
