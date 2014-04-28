b28ed8c9dd26ec5152a17713c3f9055e3ebfe3f3
DivDE <divde@free.fr>
2014-04-28 23:34:27 +0200

    Updating noder-js and hashspace versions.

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -d node_modules/hashspace ] ; then
-	git clone --depth 1 https://github.com/divdavem/hashspace.git -b nullOrUndefined node_modules/hashspace &&
+	git clone --depth 1 https://github.com/ariatemplates/hashspace.git node_modules/hashspace &&
 	( 
 		cd node_modules/hashspace &&
 		npm install grunt-cli &&
@@ -9,11 +9,3 @@ if ! [ -d node_modules/hashspace ] ; then
 	)
 fi
 
-if ! [ -d node_modules/noder-js ] ; then
-	git clone --depth 1 https://github.com/ariatemplates/noder-js.git node_modules/noder-js &&
-	( 
-		cd node_modules/noder-js &&
-		npm install grunt-cli &&
-		npm install --production=false
-	)
-fi
