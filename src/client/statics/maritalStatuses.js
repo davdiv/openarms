7c078200d1d4d1acec1f11ea21ac251eb804b109
DivDE <divde@free.fr>
2014-05-01 10:55:04 +0200

    Adding marital status.

---
+++ src/client/statics/maritalStatuses.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "single" : "Célibataire",
+    "married" : "Marié(e)",
+    "widowed" : "Veuf(ve)",
+    "concubinage" : "En concubinage",
+    "separated" : "Séparé(e)",
+    "divorced" : "Divorcé(e)"
+};
