0ab2c291dfed938dbbdad79ff9d67154106f074b
DivDE <divde@free.fr>
2014-05-03 21:35:00 +0200

    Using the latest fixes on hashspace.

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! [ -d node_modules/hashspace ] ; then
-	git clone --depth 1 https://github.com/ariatemplates/hashspace.git node_modules/hashspace &&
+	git clone --depth 1 https://github.com/divdavem/hashspace.git -b latest node_modules/hashspace &&
 	( 
 		cd node_modules/hashspace &&
 		npm install grunt-cli &&
