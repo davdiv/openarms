39ef1b49209b04f596e39c4c544e8538842d4d50
DivDE <divde@free.fr>
2014-05-05 10:26:02 +0200

    Adding icons.

--- src/common/model/account/sheet/recompute.js
+++ src/common/model/account/sheet/recompute.js
@@ -2,9 +2,11 @@ module.exports = function (data) {
     var allDays = 0;
     data.days.forEach(function (day) {
         var allLines = 0;
-        day.lines.forEach(function (line) {
-            allLines += line.amount;
-        });
+        if (day.lines) {
+            day.lines.forEach(function (line) {
+                allLines += line.amount;
+            });
+        }
         day.amount = allLines;
         allDays += allLines;
     });
