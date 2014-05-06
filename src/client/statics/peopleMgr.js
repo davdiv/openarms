dbb34f2b924ac254a70f1a103e460007e16fb798
DivDE <divde@free.fr>
2014-05-06 13:46:44 +0200

    Now: (peopleMgr instanceof RestCache) === true

--- src/client/statics/peopleMgr.js
+++ src/client/statics/peopleMgr.js
@@ -1,23 +1,15 @@
+var klass = require("hsp/klass");
 var RestCache = require("./restCache");
-var server = require("./server");
 
-var cacheInstance = new RestCache("people");
-
-// Fill the cache with some people (for demo purposes):
-cacheInstance.setItemContent({id:"pierredupont",firstName:"Pierre",sex:"male",lastName:"Dupont"});
-cacheInstance.setItemContent({id:"mariedurand",firstName:"Marie",lastName:"Durand", sex:"female"});
-
-module.exports = {
-    refreshItemContent : cacheInstance.refreshItemContent.bind(cacheInstance),
-    refreshIdentity : cacheInstance.refreshItemContent.bind(cacheInstance),
-    getItemContent : cacheInstance.getItemContent.bind(cacheInstance),
-    getIdentity : cacheInstance.getItemContent.bind(cacheInstance),
-    saveIdentity : cacheInstance.saveItemContent.bind(cacheInstance),
-    saveItemContent : cacheInstance.saveItemContent.bind(cacheInstance),
+var PeopleCache = klass({
+    $extends : RestCache,
+    $constructor : function() {
+        RestCache.$constructor.call(this, "people");
+    },
     search : function(text) {
         // TODO: request the server instead of looking in the cache
         var res = [];
-        var items = cacheInstance.items;
+        var items = this.items;
         for ( var key in items) {
             if (items.hasOwnProperty(key)) {
                 var curItem = items[key];
@@ -28,4 +20,6 @@ module.exports = {
         }
         return res;
     }
-};
+});
+
+module.exports = new PeopleCache();
