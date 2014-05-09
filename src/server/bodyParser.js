748509896040cffdb532a039991f5d8fd8b4669c
DivDE <divde@free.fr>
2014-05-09 13:26:00 +0200

    Fixing issue in body parser.

--- src/server/bodyParser.js
+++ src/server/bodyParser.js
@@ -1,16 +1,20 @@
 var rawBody = require("raw-body");
 var serialization = require('../common/serialization');
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
     rawBody(req, {
         length : req.headers["content-length"],
         limit : "1mb",
         encoding : "utf8"
-    }, function(err, string) {
+    }, function (err, string) {
         if (err) {
             return next(err);
         }
-        req.body = serialization.parse(string);
-        next();
+        try {
+            req.body = serialization.parse(string);
+            next();
+        } catch (err) {
+            next(err);
+        }
     })
 };
