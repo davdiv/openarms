b619f54479ead6c571a4716bc5d2b2b714d5e0e8
DivDE <divde@free.fr>
2014-05-18 00:22:01 +0200

    Fixing issue with formatCurrency (wrong sign for numbers between -1 and 0).

--- src/client/statics/utils/formatCurrency.js
+++ src/client/statics/utils/formatCurrency.js
@@ -2,12 +2,16 @@ module.exports = function(value) {
     if (isNaN(value)) {
         return "";
     }
+    var negative = value < 0;
+    if (negative) {
+        value = -value;
+    }
     var noDot = Math.round(value * 100);
     var afterDot = noDot % 100;
     var beforeDot = (noDot - afterDot) / 100;
-    var afterDotStr = Math.abs(afterDot) + "";
+    var afterDotStr = afterDot + "";
     if (afterDotStr.length == 1) {
         afterDotStr = "0" + afterDotStr;
     }
-    return beforeDot + "." + afterDotStr + " €";
+    return (negative ? "-" : "") + beforeDot + "." + afterDotStr + " €";
 };
