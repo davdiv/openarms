5076e2a0cd660dd6a53ec341dced949222705831
DivDE <divde@free.fr>
2014-10-15 22:58:27 +0200

    Updating hashspace.

--- preinstall.sh
+++ preinstall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if ! [ -d node_modules/hashspace ] ; then
-	git clone --depth 1 https://github.com/ariatemplates/hashspace.git node_modules/hashspace &&
+	git clone --depth 1 https://github.com/jakub-g/hashspace.git --branch=htmlsyntax-part3 node_modules/hashspace &&
 	( 
 		cd node_modules/hashspace &&
 		npm install grunt-cli &&
-		npm install --production=false
+		npm_config_production=false npm install &&
+		./node_modules/.bin/grunt package
 	)
 fi
 
